7e07feac3f21a0a9b523a2342307d27297859548
Fernando Gazzana <fernando.gazzana@gmail.com>
2025-09-22 18:58:32 -0300

    added header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,27 @@
+import strings from "../../constants/strings.json"
+import "./header.scss"
+
+export default function Header() {
+
+    const buttons: any[] = [];
+    strings.header.map((e) => {
+        buttons.push(e);
+    });
+
+    return (
+        <div className="app-header">
+
+            <img src="logo.png" alt="Alberto versão suricato"/>
+            <div className="header-button-container">
+                {
+                    buttons.map((e) => {
+                        return (
+                            <a className="header-button" href="">{e.name}</a>
+                        );
+                    })
+                }
+            </div>
+
+        </div>
+    );
+}
