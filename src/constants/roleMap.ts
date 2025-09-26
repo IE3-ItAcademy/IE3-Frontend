abd18ad3c59c7e130a2c6ca83612a30a99a1e114
alberto <albertoborsatto@hotmail.com>
2025-09-26 11:31:54 -0300

    translating employee roles

--- src/constants/roleMap.ts
+++ src/constants/roleMap.ts
@@ -1,15 +1,22 @@
-import { projects as stringsProjects } from "./strings.json"
+import { projects as stringsProjects } from "./strings.json";
 
 export const roleClassMap: Record<string, string> = {
-    [stringsProjects.dev]: "dev",
-    [stringsProjects.security]: "security",
-    [stringsProjects.manager]: "manager",
-    [stringsProjects.qa]: "qa",
-}
+  [stringsProjects.dev]: "dev",
+  [stringsProjects.security]: "security",
+  [stringsProjects.manager]: "manager",
+  [stringsProjects.qa]: "qa",
+};
 
 export const roleMap: Record<string, string> = {
-    [stringsProjects.dev]: "Dev",
-    [stringsProjects.security]: "Security",
-    [stringsProjects.manager]: "Manager",
-    [stringsProjects.qa]: "QA",
-}
+  [stringsProjects.dev]: "Dev",
+  [stringsProjects.security]: "Security",
+  [stringsProjects.manager]: "Manager",
+  [stringsProjects.qa]: "QA",
+};
+
+export const roleMapTranslation: Record<string, string> = {
+  [stringsProjects.dev]: "Desenvolvedor",
+  [stringsProjects.security]: "Segurança",
+  [stringsProjects.manager]: "Gerente",
+  [stringsProjects.qa]: "QA",
+};
